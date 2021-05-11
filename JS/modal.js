0031edbf7a7017257e3202c066988eff93d33cc2
RManchon <erasme666@gmail.com>
2021-05-11 08:03:43 +0200

    First commit. New files & folders organization. Add of closeModal function

--- JS/modal.js
+++ JS/modal.js
@@ -11,6 +11,7 @@ function editNav() {
 const modalbg = document.querySelector(".bground");
 const modalBtn = document.querySelectorAll(".modal-btn");
 const formData = document.querySelectorAll(".formData");
+const closeBtn = document.getElementsByClassName('close');
 
 // launch modal event
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
@@ -19,5 +20,14 @@ modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
 function launchModal() {
   modalbg.style.display = "block";
 }
+
+// close modal
+closeBtn[0].addEventListener("click", closeModal);
+function closeModal() {
+  modalbg.style.display = "none";
+}
+
+
+
 
 
