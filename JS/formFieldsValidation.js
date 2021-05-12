a281d1ec4a8b9814bb2fd55fbc0c6ec644ac431f
RManchon <erasme666@gmail.com>
2021-05-12 07:46:05 +0200

    Implementation of form entries and adds of validations

---
+++ JS/formFieldsValidation.js
@@ -0,0 +1,133 @@
+// DOM Elements Form Fields Validation
+const firstName = document.getElementById('first');
+const lastName = document.getElementById('last');
+const email = document.getElementById('email');
+const quantity = document.getElementById('quantity');
+const birthdate = document.getElementById('birthdate');
+const allLocations = document.getElementById('allLocations');
+const locations = document.querySelectorAll('#allLocations .checkbox-input');
+const checkbox1 = document.getElementById('checkbox1');
+const input = document.getElementsByClassName('text-control');
+const form = document.getElementById('form');
+const regex = /^[a-zA-ZÀ-ÖØ-öø-ÿ]+$/;
+
+// ------ Form Fields Validation ------ //
+// Names Check (First Name and Last Name)
+function checkFirstName() {
+    if (firstName.value.trim().length < 2 || first.value.trim() === '' || !firstName.value.match(regex)) {
+        firstName.parentElement.setAttribute('data-error-visible', 'true');
+        firstName.style.border = '2px solid #e54858';
+        return false;
+    }
+    first.parentElement.setAttribute('data-error-visible', 'false');
+    first.style.border = 'solid #279e7a 0.19rem';
+    return true;
+}
+
+function checkLastName() {
+    if (lastName.value.trim().length < 2 || last.value.trim() === "" || !lastName.value.match(regex)) {
+        lastName.parentElement.setAttribute('data-error-visible', 'true');
+        lastName.style.border = '2px solid #e54858';
+        return false;
+    }
+    last.parentElement.setAttribute('data-error-visible', 'false');
+    last.style.border = 'solid #279e7a 0.19rem';
+    return true;
+}
+
+// Email Check
+function checkEmail() {
+    const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    if (email.value.trim().match(re)) {
+        email.parentElement.setAttribute('data-error-visible', 'false');
+        email.style.border = 'solid #279e7a 0.19rem';
+        return true;
+    }
+    email.parentElement.setAttribute('data-error-visible', 'true');
+    email.style.border = '2px solid #e54858';
+    return false;
+}
+
+// Birth Date Check
+function checkBirthdate() {
+    if (birthdate.value.trim().length !== 10) {
+        birthdate.parentElement.setAttribute('data-error-visible', 'true');
+        birthdate.style.border = '2px solid #e54858';
+        return false;
+    }
+    birthdate.parentElement.setAttribute('data-error-visible', 'false');
+    birthdate.style.border = 'solid #279e7a 0.19rem';
+    return true;
+}
+
+// Number of Tournaments Check
+function checkTournamentsQuantity() {
+    if (quantity.value.trim().length === 0 || isNaN(quantity.value.trim()) === true || quantity.value.trim() < 0) {
+        quantity.parentElement.setAttribute('data-error-visible', 'true');
+        quantity.style.border = '2px solid #e54858';
+        return false;
+    }
+    quantity.parentElement.setAttribute('data-error-visible', 'false');
+    quantity.style.border = 'solid #279e7a 0.19rem';
+    return true;
+}
+
+//Locations Check
+function checkLocations() {
+    allLocations.setAttribute('data-error-visible', 'true');
+    for (let i = 0; i < locations.length; i++) {
+        if (locations[i].checked) {
+            allLocations.setAttribute('data-error-visible', 'false');
+            return true;
+        }
+    }
+    return false;
+}
+
+// Terms of use check
+function checkCheckBox() {
+    if (checkbox1.checked === false) {
+        checkbox1.parentElement.setAttribute('data-error-visible', 'true');
+        return false;
+    }
+    checkbox1.parentElement.setAttribute('data-error-visible', 'false');
+    return true;
+}
+
+// Form Fields to validate
+function formFieldsValidation(element, method, event) {
+    element.addEventListener(event, method);
+}
+//Events form
+formFieldsValidation(firstName, checkFirstName, 'focusout');
+formFieldsValidation(lastName, checkLastName, 'focusout');
+formFieldsValidation(email, checkEmail, 'focusout');
+formFieldsValidation(birthdate, checkBirthdate, 'focusout');
+formFieldsValidation(quantity, checkTournamentsQuantity, 'focusout');
+formFieldsValidation(allLocations, checkLocations, 'change');
+formFieldsValidation(checkbox1, checkCheckBox, 'change');
+
+// For  All  Fields Validation
+function forAllFieldsValidation() {
+    checkFirstName()
+    checkLastName()
+    checkEmail()
+    checkBirthdate()
+    checkTournamentsQuantity()
+    checkLocations()
+    checkCheckBox()
+}
+
+function formValidation() {
+    if (checkFirstName() === true &&
+        checkLastName() === true &&
+        checkEmail() === true &&
+        checkBirthdate() === true &&
+        checkTournamentsQuantity() === true &&
+        checkLocations() === true &&
+        checkCheckBox() === true) {
+        return true;
+    }
+    return false;
+}
+
